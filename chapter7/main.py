65214902f8f8b502e258869a31f81b0c34da7131
John Snow <yzjhhe@outlook.com>
2017-08-02 21:00:36 +0800

    chapter7-Dealing with Missing Data

--- chapter7/main.py
+++ chapter7/main.py
@@ -16,9 +16,26 @@ reload(treepredict)
 tree=treepredict.buildtree(treepredict.my_data)
 
 #======Displaying the Tree =======#
-reload(treepredict)
-treepredict.printtree(tree)
+# reload(treepredict)
+# treepredict.printtree(tree)
 
 #======Graphical Display =======#
-reload(treepredict)
-treepredict.drawtree(tree,jpeg='treeview.jpg')
+# reload(treepredict)
+# treepredict.drawtree(tree,jpeg='treeview.jpg')
+
+#======Classifying New Observations=======#
+# reload(treepredict)
+# print treepredict.classify(['(direct)','USA','yes',5],tree)
+
+#==========Pruning the Tree===========#
+# reload(treepredict)
+# treepredict.prune(tree,0.1)
+# treepredict.printtree(tree)
+# treepredict.prune(tree,1.0)
+# treepredict.printtree(tree)
+
+#========Dealing with Missing Data=========#
+# reload(treepredict)
+# print treepredict.mdclassify(['google',None,'yes',None],tree)
+# print treepredict.mdclassify(['google','France',None,None],tree)
+
